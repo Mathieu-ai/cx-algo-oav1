2631e6bc67ebf0044891ad2ae85efe2c0f80f347
Mathieu Lièvre <mathieu.lievre@efrei.net>
2020-10-14 14:41:54 +0200

    Ex02-

---
+++ e04/Array.js
@@ -0,0 +1,23 @@
+const arr = [2, 2, 2, 2, 2, 4, 5, 5, 5, 9];
+
+function foo(arr) {
+  var a = [],
+    b = [],
+    prev;
+
+  arr.sort();
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] !== prev) {
+      a.push(arr[i]);
+      b.push(1);
+    } else {
+      b[b.length - 1]++;
+    }
+    prev = arr[i];
+  }
+
+  return [a, b];
+}
+
+var result = foo(arr);
+console.log('[' + result[0] + ']','[' + result[1] + ']')
